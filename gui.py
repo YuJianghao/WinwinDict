c67741aea4e382ac64aa0f46ccce760d1c268c1a
winwin <yjh2011@live.com>
2018-03-06 18:19:29 +0800

    Add high DPI support

--- gui.py
+++ gui.py
@@ -109,6 +109,13 @@ class MainWindow(QWidget):
 
 
 if __name__ == '__main__':
+        
+    if hasattr(Qt, 'AA_EnableHighDpiScaling'):
+        QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+
+    if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
+        QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+
     app = QApplication(sys.argv)
     mw = MainWindow()
     mw.show()
